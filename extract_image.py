3abf2c236df26326afbab89694839ff55ea9bd95
txdai <txdai16@gmail.com>
2023-08-22 22:06:41 +0800

    Fix image saving

--- extract_image.py
+++ extract_image.py
@@ -1,32 +1,41 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import numpy as np
 from PIL import Image
+from test import get_largest_dir_number
 import os
 import io
 
-log_dir = "./logs/dose1_20230822-041428"  # Change this to your TensorBoard log directory
-save_dir = "../example"  # Change this to where you want to save the images
+for dosage in [1, 2, 3]:
+    print(f"Extracting images for dosage {dosage}")
+    log_dir = get_largest_dir_number(dosage, "./logs")
+    log_dir = os.path.join("./logs", log_dir)
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+    save_dir = "../example"  # Change this to where you want to save the images
 
-# Iterate through all event files
-for event_file in os.listdir(log_dir):
-    event_file_path = os.path.join(log_dir, event_file)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-    # Use EventAccumulator to read the event file
-    event_acc = EventAccumulator(event_file_path)
-    event_acc.Reload()
+    size_guidance = {
+        "images": 0,  # 0 means load all images
+    }
 
-    # Iterate through all the image tags
-    for tag in event_acc.Tags()["images"]:
-        images = event_acc.Images(tag)
+    # Iterate through all event files
+    for event_file in os.listdir(log_dir):
+        event_file_path = os.path.join(log_dir, event_file)
 
-        # Iterate through the images and save them
-        for index, img in enumerate(images):
-            image_string = np.frombuffer(img.encoded_image_string, dtype=np.uint8)
-            image_data = Image.open(io.BytesIO(image_string))
-            image_path = f"{save_dir}/{tag}_{index}.png"  # Change this to the desired path
-            image_data.save(image_path)
+        # Use EventAccumulator to read the event file
+        event_acc = EventAccumulator(event_file_path, size_guidance=size_guidance)
+        event_acc.Reload()
 
-            print(f"Saved {image_path}")
+        # Iterate through all the image tags
+        for tag in event_acc.Tags()["images"]:
+            images = event_acc.Images(tag)
+
+            # Iterate through the images and save them
+            for index, img in enumerate(images):
+                image_string = np.frombuffer(img.encoded_image_string, dtype=np.uint8)
+                image_data = Image.open(io.BytesIO(image_string))
+                image_path = f"{save_dir}/{tag}_{index}.png"  # Change this to the desired path
+                image_data.save(image_path)
+
+                print(f"Saved {image_path}")
